a6a128778a225b8a0a3326f8bfdc00bec29aede4
Karl-MarkusPress <karlmarkuspolakes@gmail.com>
2024-02-07 10:28:10 +0200

    i

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CalcSeven
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
